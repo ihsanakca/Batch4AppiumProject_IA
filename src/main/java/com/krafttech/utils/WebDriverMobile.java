cb877836b0083233927146730fe3e822c4a70750
ihsan <ihsanakcair@gmail.com>
2023-09-11 01:19:57 +0300

    secon commit

--- src/main/java/com/krafttech/utils/WebDriverMobile.java
+++ src/main/java/com/krafttech/utils/WebDriverMobile.java
@@ -1,2 +1,27 @@
-package com.krafttech.utils;public class WebDriverMobile {
+package com.krafttech.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebDriverMobile {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setExperimentalOption("androidPackage", "com.android.chrome");
+            driver = new ChromeDriver(options);
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
--- src/test/java/com/krafttech/mobileWeb/MobileChromeTest.java
+++ src/test/java/com/krafttech/mobileWeb/MobileChromeTest.java
@@ -1,2 +1,24 @@
-package com.krafttech.mobileWeb;public class MobileChromeTest {
+package com.krafttech.mobileWeb;
+
+import com.krafttech.utils.WebDriverMobile;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class MobileChromeTest {
+    WebDriver driver= WebDriverMobile.getDriver();
+
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("https://www.krafttechexlab.com/login");
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+        driver.findElement(By.id("email")).sendKeys("mgezer@gmail.com");
+        driver.findElement(By.id("yourPassword")).sendKeys("Mg12345678"+ Keys.TAB+Keys.TAB+Keys.ENTER);
+
+        Thread.sleep(2000);
+
+        WebDriverMobile.closeDriver();
+    }
 }
